a21c410fdb88e4a2a8e0e081a369dd5194fd87c7
mawonde <mawondep@gmail.com>
2024-01-09 21:43:14 +0000

    Added all files

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""  Babel Instanciation  """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Config Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Hello Word
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""  Babel Instanciation  """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Configuration Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Locale language
+
+        Return:
+            Best match to the language
+    """
+    locale = request.args.get('locale', None)
+
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ Hello Word
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
